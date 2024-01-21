6975d9b9ccec219e17fb95b0470f3ad8fcd1b993
Tejas013 <tejasmadhukar6@gmail.com>
2024-01-21 23:42:35 +0530

    Added models

---
+++ models.go
@@ -0,0 +1,11 @@
+package main
+
+type NutritionInfo struct {
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+type TotalIntake struct {
+	Total []NutritionInfo `json:"total"`
+	Goal  []NutritionInfo `json:"goal"`
+}
