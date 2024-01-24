6ca8fd72879bc0984cfcd8e0f05d109455998271
Tejas013 <tejasmadhukar6@gmail.com>
2024-01-24 18:46:48 +0530

    added chi server and routes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/Tejasmadhukar/MyFitnessPal-Grafana/internal/router"
+	"github.com/go-chi/chi/v5"
 )
 
+var (
+	r = chi.NewRouter()
+)
+
+func init() {
+	router.AddRoutes(r)
+}
+
 func main() {
-	fmt.Println("Hi")
+	fs := http.FileServer(http.Dir("internal/assets/"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":80", r)
 }
---
+++ internal/router/routes.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/Tejasmadhukar/MyFitnessPal-Grafana/internal/handler"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func AddRoutes(r *chi.Mux) {
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Heartbeat("/health"))
+	r.Get("/", handler.Index)
+}
