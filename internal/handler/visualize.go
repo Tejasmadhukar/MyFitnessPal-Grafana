45a66b923c107869c444087ee7f48d8e71a8e086
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-29 00:33:11 +0530

    Completed visualize route

--- internal/handler/visualize.go
+++ internal/handler/visualize.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -13,8 +14,13 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type FinalDashboard struct {
+	SnapshotUrl string
+}
+
 func Visualize(w http.ResponseWriter, r *http.Request) {
-	filename := chi.URLParam(r, "filename") + ".csv"
+	fileID := chi.URLParam(r, "filename")
+	filename := fileID + ".csv"
 
 	_, err := os.Stat(filepath.Join(config.ASSETS_DIR, filename))
 	if errors.Is(err, os.ErrNotExist) {
@@ -30,13 +36,31 @@ func Visualize(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = grafana.AddDataSource(filename)
+	err = grafana.AddDataSource(fileID)
 	if err != nil {
-		models.SendInternalServerError(&w, "Grafana api did not respond successfully Error: \n"+err.Error())
+		models.SendInternalServerError(&w, "Grafana datasource api did not respond successfully Error: \n"+err.Error())
 		return
 	}
 
 	w.Write([]byte("successfully created datasource now make dashboard"))
+
+	snapshotUrl, err := grafana.CreateSnapShot(fileID)
+	if err != nil {
+		models.SendInternalServerError(&w, "Grafana snapshots api did not respond successfully Error: \n"+err.Error())
+	}
+
+	tmpl, err := template.ParseFiles(filepath.Join(config.ASSETS_DIR, "templates", "snapshot_iframe.html"))
+	if err != nil {
+		log.Println(err)
+		models.SendInternalServerError(&w, "HTML template couldn't be parsed")
+		return
+	}
+
+	response := &FinalDashboard{
+		SnapshotUrl: snapshotUrl,
+	}
+
+	tmpl.Execute(w, response)
 
 	go func() {
 		os.Remove(filepath.Join(config.ASSETS_DIR, filename))
